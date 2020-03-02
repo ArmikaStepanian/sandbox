9d8f1aa684d4bc0e3c0db9a30696d894db270beb
Stepanian <armikastepanian@gmail.com>
2020-03-02 08:51:07 -0500

    add update query

--- src/main/java/com/stepanian/sandbox/pureJdbc/PreparedStatementTest.java
+++ src/main/java/com/stepanian/sandbox/pureJdbc/PreparedStatementTest.java
@@ -14,11 +14,13 @@ import static com.stepanian.sandbox.pureJdbc.ConnectionManager.getConnection;
 public class PreparedStatementTest {
 
     private static String INSERT_QUERY = "INSERT INTO client (first_name, last_name, birthday) VALUES (?, ?, ?)";
-    private static String SELECT_QUERY = "SELECT * FROM client where id = ?";
+    private static String SELECT_QUERY = "SELECT * FROM client WHERE id = ?";
+    private static String UPDATE_QUERY = "UPDATE client SET last_name = ? WHERE id = ?";
 
     public static void main(String[] args) throws IOException, SQLException {
         save("John", "Doe", LocalDate.of(1986, 5, 31));
         System.out.println(findOne(1));
+        update("Bob", 5);
     }
 
     public static void save(String firstName, String lastName, LocalDate birthday) throws IOException, SQLException {
@@ -36,6 +38,15 @@ public class PreparedStatementTest {
              PreparedStatement preparedStatement = connection.prepareStatement(SELECT_QUERY)) {
             preparedStatement.setInt(1, id);
             return showResultSet(preparedStatement.executeQuery());
+        }
+    }
+
+    public static void update(String lastName, Integer id) throws IOException, SQLException {
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_QUERY)) {
+            preparedStatement.setString(1, lastName);
+            preparedStatement.setInt(2, id);
+            preparedStatement.execute();
         }
     }
 
