74935c8ead859c5f0a8ed318ed2c6d18c40b792e
Stepanian <armikastepanian@gmail.com>
2020-03-11 20:20:00 -0400

    add final

--- src/main/java/com/stepanian/sandbox/pureJdbc/util/QueryStorage.java
+++ src/main/java/com/stepanian/sandbox/pureJdbc/util/QueryStorage.java
@@ -2,9 +2,9 @@ package com.stepanian.sandbox.pureJdbc.util;
 
 public class QueryStorage {
 
-    public static String INSERT_QUERY = "INSERT INTO client (first_name, last_name, birthday) VALUES (?, ?, ?)";
-    public static String SELECT_QUERY = "SELECT * FROM client WHERE id = ?";
-    public static String UPDATE_QUERY = "UPDATE client SET last_name = ? WHERE id = ?";
-    public static String SELECT_MULTIPLE = "SELECT * FROM client";
-    public static String UPDATE_SET_PHOTO = "UPDATE client SET photo = ? WHERE id = ?";
+    public static final String INSERT_QUERY = "INSERT INTO client (first_name, last_name, birthday) VALUES (?, ?, ?)";
+    public static final String SELECT_QUERY = "SELECT * FROM client WHERE id = ?";
+    public static final String UPDATE_QUERY = "UPDATE client SET last_name = ? WHERE id = ?";
+    public static final String SELECT_MULTIPLE = "SELECT * FROM client";
+    public static final String UPDATE_SET_PHOTO = "UPDATE client SET photo = ? WHERE id = ?";
 }
